3a41a46139d2161d86cbf04048f2bca554916d55
Tanjira <hc3778if@go.minneapolis.edu>
2016-02-19 16:20:38 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        String a = "Hello Lucky, How are you doing ";
+        //String b ="Lucky";
+        char c = 'B';
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+        String lucky = "I am happy today";
+        System.out.println(lucky);
+
+
+
+
+        int b,d,e;
+        b=5;
+        d=5;
+        e=b+d;
+        System.out.println("b+d= " +b+d);
+        System.out.println("5"+"5");
+        System.out.println(5+5);
+        System.out.println("b+d= " +e);
+
+        int k,l,m,p;
+        k=1;
+        l=3;
+        m=4;
+        p=k+l+m;
+
+
+        System.out.println("total" +p);
+
+
+
+
+
+        int f,g,h;
+        f=100;
+        g=100;
+        h=f+g;
+        System.out.println("Total is " +h);
+
+        System.out.println("Enter a number");
+        int ae = input.nextInt();
+
+        System.out.println("Enter a second number");
+        int lo = input.nextInt();
+        int sum = ae + lo;
+        System.out.println("The sume is: "+ sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
